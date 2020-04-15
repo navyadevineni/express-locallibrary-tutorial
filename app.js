d9e52dc2e57b2b62104f5ae01a98422b8d873934
Navya <navyadevineni30@gmail.com>
2020-04-15 10:12:04 -0500

    final committ

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dotenv = require('dotenv');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -12,9 +13,10 @@ const compression = require('compression');
 const helmet = require('helmet');
 
 var app = express();
+dotenv.config({ path: '.env' })
 // Set up mongoose connection
 var mongoose = require('mongoose');
-const dev_db_url = 'mongodb+srv://navyadevineni:Sirivenky20@cluster0-uijqn.mongodb.net/local_library?retryWrites=true&w=majority'
+const dev_db_url = process.env.ATLAS_URI
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
@@ -34,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
