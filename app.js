f8ba062a18121772e40e0a455bc8dab2f582e474
Navya <navyadevineni30@gmail.com>
2020-05-01 14:32:51 -0500

    updated

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/catalog')
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog'); 
+const compression = require('compression');
 const dotenv = require('dotenv')
+const helmet = require('helmet');
 
-let app = express();
+//setting up database
+const app = express();
+app.use(helmet());
 dotenv.config({ path: '.env' })
+const mongoose = require('mongoose');
 
-// Set up mongoose connection
-let mongoose = require('mongoose');
-let dev_db_url = process.env.ATLAS_URI
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
+const dev_db_url = process.env.ATLAS_URI
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
-let db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
@@ -29,11 +31,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter)
+app.use('/catalog', catalogRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -52,4 +56,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-app.listen(3002, ()=>{console.log("running now.");});
