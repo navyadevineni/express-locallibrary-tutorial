f270c13aed367572ee953c2d211d2e455eb875dd
Navya <navyadevineni30@gmail.com>
2020-05-05 12:08:07 -0500

    Changed app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(function(err, req, res, next) {
   
   // Set up mongoose connection
   let mongoose = require('mongoose');
-  let dev_db_url = 'mongodb+srv://navyadevineni30:ghcghsfhkgv@cluster1-mbdj7.mongodb.net/local_library?retryWrites=true'
+  let dev_db_url = "mongodb+srv://navyadevineni:Sirivenky20@cluster0-uijqn.mongodb.net/local_library?retryWrites=true&w=majority"
   let mongoDB = process.env.MONGODB_URI || dev_db_url;
   mongoose.connect(mongoDB, { useNewUrlParser: true });
   mongoose.Promise = global.Promise;
